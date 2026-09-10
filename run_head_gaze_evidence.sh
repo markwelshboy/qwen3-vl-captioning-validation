4e755ccb09f7ae444a783d5e78bf58431b7c4e16
markwelshboy <mark.david.richards@gmail.com>
2026-09-10 10:55:18 -0700

    Run head gaze evidence v0.2

--- run_head_gaze_evidence.sh
+++ run_head_gaze_evidence.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 PYTHON_BIN="${PYTHON_BIN:-python3}"
-exec "$PYTHON_BIN" -m qwen_caption_validate.head_gaze_evidence "$@"
+exec "$PYTHON_BIN" -m qwen_caption_validate.head_gaze_evidence_v2 "$@"
